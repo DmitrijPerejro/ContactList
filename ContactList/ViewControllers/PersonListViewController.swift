90cfee5fe0a5bcd5f885b92df7db6c6b3987bc99
perejro <perejro@centro.team>
2024-11-21 21:23:09 +0200

    remove unuse return

--- ContactList/ViewControllers/PersonListViewController.swift
+++ ContactList/ViewControllers/PersonListViewController.swift
@@ -16,15 +16,15 @@ final class PersonListViewController: UITableViewController {
 // MARK: UITableViewDataSource
 extension PersonListViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return persons.count
+        persons.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        2
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return persons[section].fullName
+        persons[section].fullName
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
